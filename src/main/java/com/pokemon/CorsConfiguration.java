b1a926488cd071106464ace8e77039c1bf728a2e
Manoel Vitor <manoelvitor4g2@gmail.com>
2021-04-07 16:14:13 -0300

    Teste

--- src/main/java/com/pokemon/CorsConfiguration.java
+++ src/main/java/com/pokemon/CorsConfiguration.java
@@ -8,22 +8,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
-	/*@Override
+	
+
+	@Override
+	   public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**");
+	    }
+	
+	/* public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**").allowedOrigins("http://api-pokemon-fatec.herokuapp.com/");
+		}
+	
+	
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("http://localhost:8080/elementos").allowedOrigins("http://localhost:8080")
 		.allowedMethods(
 				"GET", "POST", "PUT","DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-	}*/
+	}
 	
-	 @Override
-	   /* public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**");
-	    }
-*/
 	 
-	 public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**").allowedOrigins("http://api-pokemon-fatec.herokuapp.com/");
-		}
+	 */
 	
 
 }
