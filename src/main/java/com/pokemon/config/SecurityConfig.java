f3ff9d70f85c838ed85cdf5a3dc46b45db7d5e94
Manoel Vitor <manoelvitor4g2@gmail.com>
2021-06-10 00:51:39 -0300

    refazendo banco

--- src/main/java/com/pokemon/config/SecurityConfig.java
+++ src/main/java/com/pokemon/config/SecurityConfig.java
@@ -41,8 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS).permitAll()
 				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_DELETE).permitAll()
-				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_PUT).permitAll()
+				.antMatchers(HttpMethod.DELETE, PUBLIC_MATCHERS_DELETE).permitAll()
+				.antMatchers(HttpMethod.PUT, PUBLIC_MATCHERS_PUT).permitAll()
 				.antMatchers("/v3/api-docs/**", "/swagger-ui/**",
 						"/swagger-ui.html").permitAll()
 				.anyRequest().authenticated();
