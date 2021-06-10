84fa2fe61a980c80423eddf6f19cd5a9c66ce80b
Manoel Vitor <manoelvitor4g2@gmail.com>
2021-06-10 01:41:37 -0300

    mudançass

--- src/main/java/com/pokemon/config/SecurityConfig.java
+++ src/main/java/com/pokemon/config/SecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.pokemon.security.JWTAuthenticationFilter;
 import com.pokemon.security.JWTUtil;
@@ -23,30 +29,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private JWTUtil jwtUtil;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
 
-	private static final String[] PUBLIC_MATCHERS = { "/regioes/**","/pokemons/**", "/elementos/**",  "/usuarios/**" ,"/upload/**","/documentacao/**","/swagger-ui/**"};
+	private static final String[] PUBLIC_MATCHERS = { "/regioes/**", "/pokemons/**", "/elementos/**", "/usuarios/**",
+			"/upload/**", "/documentacao/**", "/swagger-ui/**" };
 
-	private static final String[] PUBLIC_MATCHERS_POST = { "/regioes/**","/pokemons/**","/elementos/**",  "/usuarios/**", "/upload/**" };
-		
-	private static final String[] PUBLIC_MATCHERS_DELETE = { "/regioes/**","/pokemons/**","/elementos/**",  "/usuarios/**", "/upload/**" };
+	private static final String[] PUBLIC_MATCHERS_POST = { "/regioes/**", "/pokemons/**", "/elementos/**",
+			"/usuarios/**", "/upload/**" };
 
-	private static final String[] PUBLIC_MATCHERS_PUT = { "/regioes/**","/pokemons/**","/elementos/**",  "/usuarios/**", "/upload/**" };
+	private static final String[] PUBLIC_MATCHERS_DELETE = { "/regioes/**", "/pokemons/**", "/elementos/**",
+			"/usuarios/**", "/upload/**" };
+
+	private static final String[] PUBLIC_MATCHERS_PUT = { "/regioes/**", "/pokemons/**", "/elementos/**",
+			"/usuarios/**", "/upload/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS).permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS).permitAll()
 				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 				.antMatchers(HttpMethod.DELETE, PUBLIC_MATCHERS_DELETE).permitAll()
 				.antMatchers(HttpMethod.PUT, PUBLIC_MATCHERS_PUT).permitAll()
-				.antMatchers("/v3/api-docs/**", "/swagger-ui/**",
-						"/swagger-ui.html").permitAll()
-				.anyRequest().authenticated();
-
+				.antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll().anyRequest()
+				.authenticated();
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
@@ -61,6 +68,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
 	}
 
 }
