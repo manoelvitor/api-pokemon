461b3c145d63e0f7038c88f3b9498d2292e5947a
manoelvitor <61368426+manoelvitor@users.noreply.github.com>
2021-10-31 08:38:25 -0300

    Update SwaggerConfig.java

--- src/main/java/com/pokemon/config/SwaggerConfig.java
+++ src/main/java/com/pokemon/config/SwaggerConfig.java
@@ -11,10 +11,10 @@ import io.swagger.v3.oas.models.info.License;
 @Configuration
 public class SwaggerConfig {
 	@Bean
-	 public OpenAPI filmesOpenAPI() {
+	 public OpenAPI pokemonOpenAPI() {
 	 return new OpenAPI().info(new Info()
 	 .title("Pokemon API")
-	 .description("Api desenvolvida para avaliação do 2º semestre da matéria Desenvolvimento para Servidores II")
+	 .description("Api desenvolvida para avaliação do 2º bimestre da matéria Desenvolvimento para Servidores II")
 	 .version("v0.0.1")
 	 .contact(new Contact()
 	 .name("Manoel Vitor da Silva").email("manoelvitor4g2@gmail.com"))
